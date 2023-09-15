23945112213f1b1c8b7db5d2206dba754f9200c2
DAVIN HENRY MONTEIRO <davinmonteiro@gmail.com>
2023-09-15 19:27:13 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import ReactDOM from "react-dom/client";
 import { Routes, Route, Link } from "react-router-dom";
 import Signup from "./components/Signup";
 import Navbar from "./components/Navbar";
-import BookList from "./components/BookList";
 import "./index.css";
 
 export default function App() {
@@ -11,7 +10,7 @@ export default function App() {
       <Navbar></Navbar>
       <Routes>
         <Route path="/" element={<Signup />}></Route>
-        <Route path="/books" element={<BookList />}></Route>
+        
       </Routes>
     </>
   );
